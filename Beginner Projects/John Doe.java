aecc51ea6bef22d9f089f4fbc63dc50fe384d587
A. Yahya Sirin <154722860+gapsedf@users.noreply.github.com>
2024-07-02 11:20:29 +0300

    Beginner Projects Directory Uploaded
    In this directory I try to learn Java. It's my first experience with Java programming language.

---
+++ Beginner Projects/John Doe.java
@@ -0,0 +1,26 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		
+		// Student Data
+		String name = "John";
+		String surname = "Doe";
+		int studentID = 1001001010;
+		char bloodType = 'X';
+		
+		
+		// Output Commands
+		System.out.println("#Student Data");
+		System.out.print(name);
+		System.out.print(" ");
+		System.out.print(surname);
+		System.out.println();
+		System.out.println("ID is : " + studentID);
+		System.out.println("Bloodtype is : " + bloodType);
+		
+		
+	}
+
+}
